a45d733959e092f6b58f09c11ac11b0bd7ceea41
김상혁 <shkim@iea.co.kr>
2025-07-17 00:07:53 +0900

    타이틀 뒤로가기 적용

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from "react-router";
 
 const Title = (props) => {
+  const navigate = useNavigate();
+
   return (
     <div className="title" data-progress={props.percents}>
       <div className="inner">
-        <button className="icon back" />
+        <button className="icon back" onClick={() => {navigate(-1)}} />
         <h2>{props.titleText}</h2>
       </div>
     </div>
