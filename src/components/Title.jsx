b172c292e27e622c6d508cd2ce2c845c9be98257
GwonBoemJuns <bj.gwon@iea.co.kr>
2025-07-17 21:22:05 +0900

    뒤로가기 ㅂㅂ

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,7 +7,7 @@ const Title = (props) => {
   return (
     <div className="title" data-progress={props.percents}>
       <div className="inner">
-        <button className="icon back" onClick={() => {navigate(-1)}} />
+
         <h2>{props.titleText}</h2>
       </div>
     </div>
