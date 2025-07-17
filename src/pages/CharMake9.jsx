4fbae6b478749389f739e55d8185f1365c1ae5bf
GwonBoemJuns <bj.gwon@iea.co.kr>
2025-07-17 16:29:24 +0900

    인쇄 후 로직 추가

--- src/pages/CharMake9.jsx
+++ src/pages/CharMake9.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Title from "../components/Title.jsx";
 import Slider from "../components/Slider.jsx";
 import qr from "../assets/img/sample_qr.png";
-import {useLocation} from "react-router";
+import {useLocation, useNavigate} from "react-router";
 import axios from "axios";
 
 const CharMake9 = () => {
@@ -11,10 +11,13 @@ const CharMake9 = () => {
     console.log("9",state)
   const [btnActive, setBtnActive] = useState(false);
   const [qrImg, setQrImg] = useState("");
+  const [printComplete, setPrintComplete] = useState(false);
+  const [moveCount, setMoveCount] = useState(0);
   const [slideList,setSideList] =useState( [
       { name: " 리아와 친구들은 도서관에서 빛나는 책을 발견했어요. 호기심에 책을 펼친 순간 황금빛 글자들이 반짝이면서 속삭였어요. “세계를 푸르게 되돌리고 싶은 자여.....” 아이들은 책을 둘러싸고 읽기 시작했어요. 각 페이지마다 동물들과 자연의 이야기가 그려져 있었어요. “옛날에는 지구가 푸른색이라는 말이 거짓이 아니었어!” 책의 마지막 장에는 ‘마법의 씨앗을 찾으면 지구를 되돌릴 수 있다’는 이야기가 적혀있었어요.", imgName: "japanese_style.png" },
       { name: " 리아와 친구들은 도서관에서 빛나는 책을 발견했어요. 호기심에 책을 펼친 순간 황금빛 글자들이 반짝이면서 속삭였어요. “세계를 푸르게 되돌리고 싶은 자여.....” 아이들은 책을 둘러싸고 읽기 시작했어요. 각 페이지마다 동물들과 자연의 이야기가 그려져 있었어요. “옛날에는 지구가 푸른색이라는 말이 거짓이 아니었어!” 책의 마지막 장에는 ‘마법의 씨앗을 찾으면 지구를 되돌릴 수 있다’는 이야기가 적혀있었어요.", imgName: "japanese_style.png" },
   ]);
+    const navigate = useNavigate();
   useEffect(() => {
   //     state 책 보여주기
       preProcess()
@@ -33,6 +36,7 @@ const CharMake9 = () => {
         setSideList(storyList)
     }
     const printCard = async () => {
+      setBtnActive(true);
         let job="";
         state.slideList.forEach(element => {
             if(element.status){
@@ -40,8 +44,7 @@ const CharMake9 = () => {
             }
         })
 
-
-      axios.post( "http://localhost:8080/user/book/print",{
+      await axios.post( "http://localhost:8080/user/book/print",{
           hope:job,
           name:state.name,
           charc_link:state.charInfo.img_url,
@@ -53,12 +56,21 @@ const CharMake9 = () => {
               "Content-Type": "application/json"
           }
       })
+        setPrintComplete(true);
+        setBtnActive(false);
+        setTimeout(() => {
+            setMoveCount(moveCount+1);
+        }, 1000);
+
+        if(moveCount >= 3){
+            navigate("/");
+        }
     }
 
   return (
     <div className="make-finished">
       <Title titleText="제작 완료" percents={100} />
-      <h3>나만의 동화가 완성됐어요!</h3>
+      <h3>{!printComplete ? "나만의 동화가 완성됐어요!" : "인쇄가 완료되었습니다!"}</h3>
       <Slider
         className={"fairytale-reader"}
         slideList={slideList}
@@ -69,7 +81,16 @@ const CharMake9 = () => {
         type={"bookReader"}
       />
       <div className="btn-wrap">
-        <button onClick={printCard} className={`btn print ${!btnActive ? "" : "disabled"}`}>프린트하기</button>
+          {!printComplete ? 
+              <>
+                  <button onClick={printCard} className={`btn print ${!btnActive ? "" : "disabled"}`}>
+                      {!btnActive ? "프린트하기" : "인쇄 중입니다..."}
+                  </button>
+              </> :
+              <>
+                <p className="move-count"><span>{moveCount}초</span> 후 처음 화면으로 돌아갑니다.</p>
+              </>
+          }
       </div>
       <div className="qr-wrap">
         <p>동화 다운로드 QR</p>
